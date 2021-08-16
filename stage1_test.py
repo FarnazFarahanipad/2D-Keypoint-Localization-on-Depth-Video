5e5a549bbf85483c33ade7e469c67c1cb67ec739
Farnaz Farahanipad <50027513+FarnazFarahanipad@users.noreply.github.com>
2021-08-16 17:57:23 -0500

    Create stage1_test.py

---
+++ stage1_test.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import fractions
+from collections import OrderedDict
+from torch.autograd import Variable
+
+
+class CustomDatasetDataLoader(BaseDataLoader):
+    def name(self):
+        return 'CustomDatasetDataLoader'
+
+    def initialize(self, opt):
+        BaseDataLoader.initialize(self, opt)
+        self.dataset = CreateDataset(opt)
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batchSize,
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.nThreads))
+
+    def load_data(self):
+        return self.dataloader
+
+    def __len__(self):
+        return min(len(self.dataset), self.opt.max_dataset_size)
+
+def CreateDataLoader(opt):
+    data_loader = CustomDatasetDataLoader()
+    print(data_loader.name())
+    data_loader.initialize(opt)
+    return data_loader
