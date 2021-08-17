79af34e1431fccb26907d17bda3819c02bfe512d
Farnaz Farahanipad <50027513+FarnazFarahanipad@users.noreply.github.com>
2021-08-17 12:49:24 -0500

    Update stage1_test.py

--- stage1_test.py
+++ stage1_test.py
@@ -13,34 +13,6 @@ from models.models import create_model
 from util.test_options import TestOptions
 
 
-class CustomDatasetDataLoader(BaseDataLoader):
-    def name(self):
-        return 'CustomDatasetDataLoader'
-
-    def initialize(self, opt):
-        BaseDataLoader.initialize(self, opt)
-        self.dataset = CreateDataset(opt)
-        self.dataloader = torch.utils.data.DataLoader(
-            self.dataset,
-            batch_size=opt.batchSize,
-            shuffle=not opt.serial_batches,
-            num_workers=int(opt.nThreads))
-
-    def load_data(self):
-        return self.dataloader
-
-    def __len__(self):
-        return min(len(self.dataset), self.opt.max_dataset_size)
-
-def CreateDataLoader(opt):
-    data_loader = CustomDatasetDataLoader()
-    print(data_loader.name())
-    data_loader.initialize(opt)
-    return data_loader
-
-
-
-
 opt = TestOptions().parse(save=False)
 opt.nThreads = 1   # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
